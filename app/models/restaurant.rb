a31f5a575f233019d6296b7a4141c010b297a30c
Chris Childress <cchildre@westga.edu>
2019-03-22 21:46:42 -0400

    added validation to model class

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :streetaddress, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true, format: { with: /\d{5}/, message: "Zip Code must contain 5 numbers" }
+  validates_numericality_of :upvotes, only_integer: true, greater_than_or_equal_to:  0
+  validates_numericality_of :downvotes, only_integer: true, greater_than_or_equal_to: 0
 end
