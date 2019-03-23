e1b7016705832309f0af622f3afb4a56a4f3c182
Chris Childress <cchildre@westga.edu>
2019-03-22 20:10:45 -0400

    added root index as restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'restaurants#index', as: 'restaurants_index'
   resources :restaurants, except: :destroy
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
