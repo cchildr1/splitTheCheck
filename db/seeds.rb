dd7d4b69cf04b11a286703e1f918e221be3f67f7
Chris Childress <cchildre@westga.edu>
2019-03-22 20:00:13 -0400

    added csv to represent data and method to read in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+require 'csv'
+
+csv_text = File.read('/lib/data/data.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  Restaurant.create!(row.to_hash)
+end
