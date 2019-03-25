f3aeee1f4c3a6d9f3f78219a9377d847d3d36b01
Chris Childress <cchildre@westga.edu>
2019-03-25 19:18:52 -0400

    minor changes to forms, added classes to images for testing, wrote system tests to verify that images change appropriately when upvoted and downvoted.

--- test/system/restaurants_test.rb
+++ test/system/restaurants_test.rb
@@ -15,12 +15,10 @@ class RestaurantsTest < ApplicationSystemTestCase
     click_on "New Restaurant"
 
     fill_in "City", with: @restaurant.city
-    fill_in "Downvotes", with: @restaurant.downvotes
     fill_in "Name", with: @restaurant.name
     fill_in "State", with: @restaurant.state
-    fill_in "Streetaddress", with: @restaurant.streetaddress
-    fill_in "Upvotes", with: @restaurant.upvotes
-    fill_in "Zipcode", with: @restaurant.zipcode
+    fill_in "Street Address", with: @restaurant.streetaddress
+    fill_in "Zip Code", with: @restaurant.zipcode
     click_on "Create Restaurant"
 
     assert_text "Restaurant was successfully created"
@@ -29,27 +27,48 @@ class RestaurantsTest < ApplicationSystemTestCase
 
   test "updating a Restaurant" do
     visit restaurants_url
-    click_on "Edit", match: :first
-
+    click_on @restaurant.name.to_s, match: :first
+    click_on "Edit"
     fill_in "City", with: @restaurant.city
-    fill_in "Downvotes", with: @restaurant.downvotes
     fill_in "Name", with: @restaurant.name
     fill_in "State", with: @restaurant.state
-    fill_in "Streetaddress", with: @restaurant.streetaddress
-    fill_in "Upvotes", with: @restaurant.upvotes
-    fill_in "Zipcode", with: @restaurant.zipcode
+    fill_in "Street Address", with: @restaurant.streetaddress
+    fill_in "Zip Code", with: @restaurant.zipcode
     click_on "Update Restaurant"
 
     assert_text "Restaurant was successfully updated"
     click_on "Back"
   end
 
-  test "destroying a Restaurant" do
+  test "shows thumbs up when restaurant splits the check" do
+    restaurant = restaurants(:splits)
     visit restaurants_url
-    page.accept_confirm do
-      click_on "Destroy", match: :first
-    end
-
-    assert_text "Restaurant was successfully destroyed"
+    click_on restaurant.name.to_s, match: :first
+    assert page.has_css?('.splitst')
   end
+
+  test "shows thumbs down when restaurant doesn't split the check" do
+    restaurant = restaurants(:two)
+    visit restaurants_url
+    click_on restaurant.name.to_s, match: :first
+    assert page.has_css?('.splitsf')
+  end
+
+  test "shows shrug when votes are tied" do
+    visit restaurants_url
+    click_on @restaurant.name.to_s, match: :first
+    assert_text "¯\\_(ツ)_/¯"
+  end
+
+  test "upvoted restaurant changes from shrug to thumbs up" do
+    restaurant = restaurants(:reform)
+    visit restaurants_url
+    click_on restaurant.name.to_s, match: :first
+    click_on "Upvote"
+    assert_text restaurant.name.to_s + " upvoted"
+    assert_text "¯\\_(ツ)_/¯"
+    click_on "Upvote"
+    assert page.has_css?('.splitst')
+  end
+
 end
