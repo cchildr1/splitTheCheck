5639c890f5c133be9a529c143b19c6a6a6ea0f0b
Chris Childress <cchildre@westga.edu>
2019-04-17 15:11:33 -0400

    changed comment value to text instead of string, added comments resource to routes, generated comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'restaurants#index', as: 'restaurants_index'
-  resources :restaurants, except: :destroy
+  resources :restaurants, except: :destroy do
+    resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/upvote/:id", to: "restaurants#upvote", as: "restaurants_upvote"
   get "/downvote/:id", to: "restaurants#downvote", as: "restaurants_downvote"
