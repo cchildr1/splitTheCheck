c4503526696961b206a82677b363fe4e29186a06
Chris Childress <cchildre@westga.edu>
2019-04-17 15:16:01 -0400

    added comment create method in comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,8 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @comment = @restaurant.comments.create(params[:comment].permit(:content, :user_id))
+    redirect_to restaurant_path(@restaurant)
+  end
 end
